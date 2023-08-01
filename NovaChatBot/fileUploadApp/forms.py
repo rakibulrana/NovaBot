7dc68d364ab5daf1a43f88dcf96730456d21c45d
islamr <r.islam@innosep.de>
2023-08-01 21:09:05 +0200

    add File Edit and Delete option of the file tree

--- NovaChatBot/fileUploadApp/forms.py
+++ NovaChatBot/fileUploadApp/forms.py
@@ -6,3 +6,12 @@ class UploadFileForm(forms.ModelForm):
     class Meta:
         model = UploadedFile
         fields = ['name', 'file']
+
+    def clean_file(self):
+        file = self.cleaned_data.get('file')
+        if file:
+            allowed_formats = ['csv', 'txt', 'xml', 'json', 'h5']
+            ext = file.name.split('.')[-1].lower()
+            if ext not in allowed_formats:
+                raise forms.ValidationError(f"Invalid file format. Allowed formats are: {', '.join(allowed_formats)}")
+        return file
--- NovaChatBot/fileUploadApp/views.py
+++ NovaChatBot/fileUploadApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import UploadedFile
 from .forms import UploadFileForm
 
+from django.contrib import messages
+
 
 def base(request):
     return render(request, 'base.html', {'year': 2023})
@@ -9,15 +11,13 @@ def base(request):
 
 def upload_file(request):
     if request.method == 'POST':
-
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-
+            messages.success(request, "File uploaded successfully!")
             return redirect('file_list_view')
     else:
         form = UploadFileForm()
-
     return render(request, 'fileUpload/upload_file.html', {'form': form})
 
 
@@ -32,7 +32,8 @@ def edit_file(request, file_id):
         form = UploadFileForm(request.POST, instance=file)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            messages.success(request, 'File name edited successfully.')
+            return redirect('file_list_view')
     else:
         form = UploadFileForm(instance=file)
     return render(request, 'fileUpload/edit_file.html', {'form': form, 'file': file})
@@ -42,7 +43,7 @@ def delete_file(request, file_id):
     file = get_object_or_404(UploadedFile, id=file_id)
     if request.method == 'POST':
         file.delete()
-        return redirect('home')
+        return redirect('file_list_view')
     return render(request, 'fileUpload/delete_file.html', {'file': file})
 
 
