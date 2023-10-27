05d2e4e9e54d7a9df9c5f86d7066e883eba466a3
islamr <r.islam@innosep.de>
2023-10-27 13:16:39 +0200

    testing

--- NovaChatBot/ml_apps/views.py
+++ NovaChatBot/ml_apps/views.py
@@ -1,18 +1,23 @@
 from fileUploadApp.models import UploadedFile
-import xml.etree.ElementTree as ET
-import json
-from django.shortcuts import render, get_object_or_404
-import pandas as pd
+
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 
+import pandas as pd
+from django.shortcuts import render, get_object_or_404
+from ydata_profiling import ProfileReport  # Import ProfileReport from ydata_profiling
+
+profile_config = {
+    "html.navbar_show": False,
+}
+
 
 def preprocess_view(request, file_id):
     file = get_object_or_404(UploadedFile, id=file_id)
-
     file_content = None
     validation_errors = []
+    profile_html = None
 
     if file.file:
         try:
@@ -21,39 +26,54 @@ def preprocess_view(request, file_id):
 
             if file_extension == 'csv':
                 df = pd.read_csv(file.file)
-                file_content = df.values.tolist()
             elif file_extension == 'txt':
                 df = pd.read_csv(file.file, delimiter='\t')
-                file_content = df.values.tolist()
             elif file_extension == 'xml':
-                tree = ET.parse(file.file)
-                root = tree.getroot()
-                file_content = [[elem.text for elem in child] for child in root]
+                # Your XML processing code here
+                pass
             elif file_extension == 'json':
-                with open(file.file.path) as json_file:
-                    data = json.load(json_file)
-                    file_content = [[str(value) for value in row.values()] for row in data]
+                # Your JSON processing code here
+                pass
             elif file_extension == 'h5':
-                df = pd.read_hdf(file.file)
-                file_content = df.values.tolist()
+                # Your HDF processing code here
+                pass
             else:
-                validation_errors.append("Unsupported file format.")
+                validation_errors.append("Unsupported file format")
+
+            # Check for NaN values in the DataFrame and fill them if necessary
+            if df.isnull().values.any():
+                df.fillna(0, inplace=True)  # You can fill NaN values with any appropriate value
+
+            df_head = df.head()
+            file_content = df.values.tolist()
 
         except Exception as e:
-            file_content = []
             validation_errors.append(f"Error occurred while reading the file: {str(e)}")
 
     else:
         validation_errors.append("File not found.")
 
-    # Perform basic data validation checks
-    if file_content:
-        for row_index, row in enumerate(file_content):
-            if any(pd.isnull(cell) for cell in row):
-                validation_errors.append(f"Row {row_index + 1} contains null values.")
+    if request.method == "POST" and "generate_report" in request.POST:
+        # Generate the Pandas Profiling Report
+        if file_content:
+            # Create a DataFrame from your file_content
+            df = pd.DataFrame(file_content)
 
-    return render(request, 'preprocess_view.html',
-                  {'file_content': file_content, 'validation_errors': validation_errors, 'file_id': file.id})
+            title = "Pandas Profiling Report"
+
+            # Create the profile report
+            profile = ProfileReport(df)
+
+            # Convert the report to an HTML string
+            profile_html = profile.to_html()
+
+    return render(request, 'preprocess_view.html', {
+        'file_content': file_content,
+        'validation_errors': validation_errors,
+        'file_id': file.id,
+        'df_head': df_head,
+        'profile_html': profile_html,
+    })
 
 
 def signal_visualization_view(request, file_id):
